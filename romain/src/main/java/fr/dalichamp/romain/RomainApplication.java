a9b454b13db86417e7822b07dab30a6b78570630
romain <romain@JavaOnWindows.2am21rz0e0xulokio04yeyeleh.bx.internal.cloudapp.net>
2019-05-03 16:55:44 -0500

    First Commit

---
+++ romain/src/main/java/fr/dalichamp/romain/RomainApplication.java
@@ -0,0 +1,13 @@
+package fr.dalichamp.romain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomainApplication.class, args);
+	}
+
+}
