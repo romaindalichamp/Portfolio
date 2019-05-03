b8a95a4d10557ed7f77303b4930b683408c17552
romain <romain@JavaOnWindows.2am21rz0e0xulokio04yeyeleh.bx.internal.cloudapp.net>
2019-05-03 17:52:41 -0500

    commit and push to see if it is automatically updating the Heroku sources as well

--- romain/src/main/java/fr/dalichamp/romain/controllers/PagesController.java
+++ romain/src/main/java/fr/dalichamp/romain/controllers/PagesController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class PagesController {
 	
-	@GetMapping("/")
+	@GetMapping("/") //comment
 	public String homeGet(@RequestParam(required=false, defaultValue = "World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "pages/home";
