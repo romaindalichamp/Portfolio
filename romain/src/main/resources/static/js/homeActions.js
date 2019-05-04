0256e5b902038e9310116e96d2db2e50362a7c90
romain <romain@JavaOnWindows.2am21rz0e0xulokio04yeyeleh.bx.internal.cloudapp.net>
2019-05-04 12:20:51 -0500

    Finishing Design

--- romain/src/main/resources/static/js/homeActions.js
+++ romain/src/main/resources/static/js/homeActions.js
@@ -1,10 +1,12 @@
 var url_cv="http://romain.dalichamp.fr/cv_ca";
 var url_resume="http://romain.dalichamp.fr/resume_ca";
+var url_mail="mailto:romain.dalichamp@free.fr";
 
 $( ".btn" ).click(function() {	var url="";
 	switch(this.id) {
 	  case "cv": 	url = url_cv; 		break;
 	  case "resume":url = url_resume; 	break;
+	  case "mailto":url = url_mail; 	break;
 	  default:		url = url_resume; 	break;
 	} 
 	window.open(url, '_blank');
