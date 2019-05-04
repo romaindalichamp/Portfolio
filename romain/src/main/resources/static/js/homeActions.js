dc4e72691b6718cc245d9a796594ad8ccc58760a
romain <romain@JavaOnWindows.2am21rz0e0xulokio04yeyeleh.bx.internal.cloudapp.net>
2019-05-04 13:53:18 -0500

    Presentation

--- romain/src/main/resources/static/js/homeActions.js
+++ romain/src/main/resources/static/js/homeActions.js
@@ -1,8 +1,12 @@
-var url_cv="http://romain.dalichamp.fr/cv_ca";
-var url_resume="http://romain.dalichamp.fr/resume_ca";
-var url_mail="mailto:romain.dalichamp@free.fr";
+var url_cv		= "http://romain.dalichamp.fr/cv_ca";
+var url_resume	= "http://romain.dalichamp.fr/resume_ca";
+var url_mail	= "mailto:romain.dalichamp@free.fr";
 
-$( ".btn" ).click(function() {	var url="";
+var aboutMe	= "aboutMe";
+var projects= "projects";
+
+$( ".buttonOut" ).click(function() {	
+	var url="";
 	switch(this.id) {
 	  case "cv": 	url = url_cv; 		break;
 	  case "resume":url = url_resume; 	break;
@@ -10,4 +14,23 @@ $( ".btn" ).click(function() {	var url="";
 	  default:		url = url_resume; 	break;
 	} 
 	window.open(url, '_blank');
+});
+
+
+$( ".buttonAnchor" ).click(function() {	
+	var anchor="";
+	switch(this.id) {
+	  case "more": 		anchor = aboutMe; 	break;
+	  case "projects":	anchor = projects; 	break;
+	  default:			anchor = aboutMe;	break;
+	} 
+	// animate
+	$('html, body').animate({
+	    scrollTop: $('#'+anchor).offset().top
+	  }, 300, function(){
+	
+	    // when done, add hash to url
+	    // (default click behaviour)
+	    window.location.hash = anchor;
+	  });
 });
